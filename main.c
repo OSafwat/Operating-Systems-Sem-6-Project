12ca35710e37ff1d94bd2da636ff473e5be2bb27
OSafwat <omarsafsab2004@gmail.com>
2025-03-10 19:46:27 +0200

    Completed Thread 3 in the milestone

--- main.c
+++ main.c
@@ -2,16 +2,32 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void *hello(void *arg)
+typedef struct
 {
-    printf("Hello from thread!\n");
-    return NULL;
+    int a, b;
+} NumberPair;
+
+void *calculateSumAverageProduct(void *arg)
+{
+    NumberPair *value = (NumberPair *)arg;
+    int sum = (value->b * (value->b + 1)) / 2 - (value->a * (value->a - 1)) / 2;
+    int product = 1;
+    for (int i = value->a; i <= value->b; i++)
+        product *= i;
+    int average = (value->a + value->b) / 2;
+    printf("The sum, product, and average of of all integers between and including those two numbers are %d, %d, and %d respectively.\n", sum, product, average);
 }
 
 int main()
 {
+    int a, b;
+    printf("Please enter the two numerical inputs.\n");
+    scanf("%d%d", &a, &b);
+    NumberPair input;
+    input.a = a;
+    input.b = b;
     pthread_t thread;
-    pthread_create(&thread, NULL, hello, NULL);
+    pthread_create(&thread, NULL, calculateSumAverageProduct, &input);
     pthread_join(thread, NULL);
     return 0;
 }
